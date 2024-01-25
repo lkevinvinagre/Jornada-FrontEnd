f4d44c93c3625b7c02537b66d500af3e4ac06bae
kem4r1 <kevin.cv.br@gmail.com>
2024-01-25 10:03:16 -0400

    Aula02-parte1

--- ocean-jornada-frontend/src/App.jsx
+++ ocean-jornada-frontend/src/App.jsx
@@ -4,7 +4,12 @@ import './App.css'
 function App() {
 
   return (
-    <p>Aula02</p>
+    <>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' />
+      </div>
+    </>
   )
 }
 
